c0f56bdd640abe8c3d25b5e3ce58e6bde0c4b698
Harold Fellermann <harold.fellermann@newcastle.ac.uk>
2023-06-22 11:29:45 +0100

    Move mars.py into nanosuite package

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Nanosuite',
+      version='0.1',
+      description='Utilities for work with DNA nanotechnology',
+      author='Harold Fellermann',
+      author_email='harold@nanovery.co.uk',
+      url='http://harfel.teerun.de/',
+)
