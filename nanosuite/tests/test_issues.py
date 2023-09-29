d6691a2c20e3ddf5f1861ea1586bc13416be52a2
Harold Fellermann <harold.fellermann@newcastle.ac.uk>
2023-09-29 15:00:57 +0100

    Add regression test for CRN string specification

--- nanosuite/tests/test_issues.py
+++ nanosuite/tests/test_issues.py
@@ -1,5 +1,8 @@
-def test_issue_1():
-    from .. import crn
+import pytest
+import xarray as xr
+from .. import crn
+
+def test_issue_0001():
     crn_A = crn.from_string("""
         A + B -> C; k1 = 10
         C + D -> X + A; k2 = 5
@@ -16,3 +19,18 @@ def test_issue_1():
     assert crn_B.params['k1'] == 10
     assert crn_B.params['k2'] == 5
     assert crn_B.params['p'] == 0.03
+
+
+def test_issue_0002():
+    impure_crn = crn.from_string("""
+        A [impure] + B -> X
+        A + B -> Y
+    """)
+    initial = xr.DataArray(
+        [10, 10],
+        {'species': ['A', 'B']}
+    )
+    try:
+        impure_crn.integrate(initial)
+    except ValueError as x:
+        pytest.fail(str(x))
