45aee93f692e719936b00691a0eef4324cfc2866
harold <dadapapa@gmail.com>
2024-01-09 13:48:52 +0000

    Move examples into the nanosuite package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 """nanosuite - Utilities to work with DNA nanotechnology
 """
 
-from setuptools import setup
+from setuptools import setup    # type: ignore
 from nanosuite import __version__
 
 setup(name='Nanosuite',
@@ -12,9 +12,10 @@ setup(name='Nanosuite',
       author_email='harold@nanovery.co.uk',
       url='http://harfel.teerun.de/',
       packages=['nanosuite'],
+      include_package_data=True,
       install_requires=[
         "numpy",
-        "openpyxl",
+        "openpyxl", # required by pandas.read_excel
         "pandas",
         "xarray",
         "scipy",
