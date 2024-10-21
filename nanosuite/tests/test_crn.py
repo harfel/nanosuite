610f1f74e76a7c899d5cbba4abcb6a13cdb28fa5
harold <dadapapa@gmail.com>
2024-10-21 13:53:33 +0100

    Fix parsing of stoichiometric factors
    This fixes a regression bug in the CRN parser which did not allow
    to write 2A -> B, 2*A -> B, etc. any longer.

--- nanosuite/tests/test_crn.py
+++ nanosuite/tests/test_crn.py
@@ -68,6 +68,11 @@ def test_stoichiometries(reaction):
     """Ensure that species can occur in higher stoichiometries"""
     crn.from_string(reaction)
 
+def test_from_string_forbids_nonint_stoichiometries():
+    """Forbid noninteger notation of stoichiometries"""
+    with pytest.raises(ValueError):
+        crn.from_string("1.0 A -> B")
+
 def test_from_string_rate():
     """Ensure correct parsing of reaction rates"""
     test_crn = crn.from_string("""
@@ -82,7 +87,7 @@ def test_from_string_rate_name():
     """)
     assert test_crn.params['k'] == 1
 
-@pytest.mark.parametrize("value", [10, 1.0, 1e-1, 'inf'])
+@pytest.mark.parametrize("value", [10, '10.', 100, 1.0, 1e-1, 'inf'])
 def test_from_string_rate_value(value):
     """Ensure correct parsing of rate constant values"""
     test_crn = crn.from_string(f"""
@@ -178,7 +183,7 @@ def test_impurities():
     test_crn = crn.from_string("""
        A contains impure with p_impure = 0.1
 
-       A -> X;           k = 10.
+       A -> X;           k = 10
        A [impure] -> Y;  k
     """)
     initial = test_crn.state(A=1.)
