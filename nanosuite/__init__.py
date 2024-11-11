e043700ad5ce4a3127530e9398a58cd1c45eca77
harold <dadapapa@gmail.com>
2024-11-11 23:13:18 +0000

    Bump version to 0.2.4rc3

--- nanosuite/__init__.py
+++ nanosuite/__init__.py
@@ -2,8 +2,13 @@
 
 Toolset for working with experimental DNA nanotechnology.
 """
-__version__ = '0.2.4rc1'
+__version__ = '0.2.4rc3'
 
 
-from .mars import Assay
-from .crn import from_string as crn_from_string
+try:
+    from .mars import Assay
+    from .crn import from_string as crn_from_string
+
+except ImportError:
+    # occurs during installation when setup.py imports the __version__ info form __init__
+    pass
