d56955c95c3895204f99e030a492963a6e517ed9
harold <dadapapa@gmail.com>
2024-11-12 14:32:18 +0000

    Enhance usability in jupyter notebooks

--- nanosuite/__init__.py
+++ nanosuite/__init__.py
@@ -5,8 +5,23 @@ Toolset for working with experimental DNA nanotechnology.
 __version__ = '0.2.4rc3'
 
 
-try:
+def _is_notebook() -> bool:
+    """Detect if code is run in a jupyter notebook"""
+    try:
+        shell = get_ipython().__class__.__name__  # type: ignore
+        if shell == 'ZMQInteractiveShell':
+            return True   # Jupyter notebook or qtconsole
+        if shell == 'TerminalInteractiveShell':
+            return False  # Terminal running IPython
+        return False  # Other type (?)
+    except NameError:
+        return False      # Probably standard Python interpreter
+
+
+if _is_notebook():
     from . import jupyter
+
+try:
     from .mars import Assay
     from .crn import from_string as crn_from_string
 
---
+++ nanosuite/jupyter.py
@@ -0,0 +1,106 @@
+"""Usability enhancements for running nanosuite in an interactive environment
+
+This module requires IPython and matplotlib
+"""
+import base64
+from copy import deepcopy
+import io
+from typing import Any, Callable
+from IPython.display import display, HTML  # type: ignore
+import lmfit                               # type: ignore
+from matplotlib import colormaps           # type: ignore
+from matplotlib.figure import Figure       # type: ignore
+import xarray as xr                        # type: ignore
+from . import crn, mars
+
+
+####################################################################################################
+#
+# crn enhancements
+#
+####################################################################################################
+class FitProgress:
+    """Live visualization for CRN.fit"""
+    def __init__(self, crn_, data, initial, conversion, error):
+        self.crn = crn_
+        self.data = data
+        self.initial = initial[initial.sample.isin(data.sample)]
+        self.conversion = conversion or (lambda conc: conc.sel(species=data.species))
+        self.error = error
+        self.hdisplay = None
+
+    def __enter__(self):
+        self.hdisplay = display(HTML('<div/>'), display_id=True)
+        return self
+
+    def __exit__(self, typ, value, traceback):
+        pass # self.hdisplay.update(HTML('<div/>'))
+
+    def __call__(self, params, num_it, residuals, *args, **kwargs):
+        original = deepcopy(self.crn.params)
+        self.crn.params = params
+        traj = self.conversion(self.crn.integrate(self.initial, t_eval=self.data.time))
+        self.crn.params = original
+
+        def gradient(dataset):
+            size = len(dataset)
+            for idx, _ in enumerate(dataset):
+                yield colormaps['rainbow'](idx/size)
+
+        fig = Figure()
+        ax = fig.gca()
+        for experiment, model, color in zip(self.data, traj, gradient(self.data)):
+            ax.plot(experiment.time, experiment, '-', c=color)
+            ax.plot(model.time, model, '--', c=color)
+        ax.set_xlabel('Time [s]')
+        ax.set_ylabel('Concentration [M]')
+        ax.grid()
+
+        buf = io.BytesIO()
+        fig.savefig(buf, format='png')
+        buf.seek(0)
+
+        self.hdisplay.update(HTML(f"""
+        <div>
+            <div>Iteration: {num_it}</div>
+            <img src="data:image/png;base64,{base64.b64encode(buf.read()).decode()}">
+            <div style="displaY: inline-block">{params._repr_html_()}</div>
+        </div>
+        """))
+
+class CRN(crn.CRN):
+    def fit(self,
+            data: xr.DataArray,
+            initial: xr.DataArray,
+            conversion: Callable[[xr.DataArray], xr.DataArray]|None=None,
+            error: float|xr.DataArray=1.,
+            *,
+            iter_cb: Callable|None = None,
+            **options) -> lmfit.minimizer.MinimizerResult:
+        if iter_cb:
+            return super().fit(data, initial, conversion, error, iter_cb=iter_cb, **options)
+        with FitProgress(self, data, initial, conversion, error) as progress:
+            return super().fit(data, initial, conversion, error, iter_cb=progress, **options)
+
+class PartitionedCRN(crn.PartitionedCRN, CRN):
+    pass
+
+# monkey patches
+crn.CRN = CRN                        # type: ignore
+crn.PartitionedCRN = PartitionedCRN  # type: ignore
+
+
+####################################################################################################
+#
+# mars enhancements
+#
+####################################################################################################
+class Assay(mars.Assay):
+    def _repr_mimebundle_(self) -> dict[str, Any]:
+        return NotImplemented # FIXME: implement this
+
+    def _repr_html_(self) -> str:
+        return NotImplemented
+
+# monkey patches
+mars.Assay = Assay                   # type: ignore
