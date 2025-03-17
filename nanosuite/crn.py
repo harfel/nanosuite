harfel/nanosuite

commit f704ac141f60d7ad8eebecd42c4163181bd474b0

Please mypy and pylint and sort TODOs

nanosuite/crn.py
@@ -509,8 +509,8 @@ class CRN:
         # TODO: The parameter_map currently does not respect buffer and media
         or any descriptor other than chemical species.
         """
-        # We define a mapping of sample indices to parameter_map
-        # the default behaviour is for all samples to reference the parameter in self.params
+        # We define a mapping of sample indices to parameters
+        # the default behaviour is for all samples to reference the general parameter
         parameter_map = ParameterMap(sample_map)
         parameter_map.add_many(*self.params.values())
 
@@ -818,7 +818,7 @@ class PartitionedCRN(CRN):
         for species_def in species_defs or []:
             self.define_subspecies(*species_def)
 
-    def split_species(self, index: int = -1) -> np.ndarray: # FIXME: index: pd.Index instead?
+    def split_species(self, index: pd.Index|None = None) -> np.ndarray:
         """Split matrix distributing species into subspecies concentrations"""
         all_subspecies = set(chain(*self.subspecies.values()))
 
@@ -826,8 +826,8 @@ class PartitionedCRN(CRN):
             if species in self.subspecies:
                 if subspecies in self.subspecies[species]:
                     name = self.subspecies[species][subspecies]
-                    if index != -1 and len(self.params.mapping):
-                        name = self.params.mapping[name].iloc[index]
+                    if index is not None and len(self.params.mapping):
+                        name = str(self.params.mapping[name].loc[index])
                     return self.params[name]
                 return 0.
             return int(species==subspecies and subspecies not in all_subspecies)
@@ -892,8 +892,9 @@ class PartitionedCRN(CRN):
 
         if len(state.dims) == 1:
             return xr.DataArray(state.values @ self.split_species(), state.coords)
-        return xr.DataArray([state.values[idx] @ self.split_species(idx)
-                                 for idx, sample in enumerate(state)],
+        content_dim = next(iter(state.coords))
+        return xr.DataArray([sample.values @ self.split_species(state.coords[content_dim].data)
+                                 for sample in state],
                                 state.coords)
 
     def equilibrate(self, initial_condition: xr.DataArray|dict, **options) -> xr.DataArray:

TODO
@@ -1,14 +1,11 @@
-- make controls optional for some or all samples
-- allow buffer, media, or any non-species dependencies in CRN.parmeter_map(rate_dependencies)
-* write tests!
-= feature/parameter_classes
 * maintain CRN reversibility when rate constants are initialized with 0
-- allow to turn jupyter interactive mode on or off
-- inbuilt conversion against controls
-- simplify RFU conversion
+- simplify RFU conversion -- supporting multiple controls
 * make CRN.equilibrate accept the same argument types as CRN.integrate [feature-crn-equilibrate]
+- simplify assignment of specialized parameters, e.g. from value sequence
+- allow to turn jupyter interactive mode on or off
 - fix doctests
 = 0.3.0
+- allow buffer, media, or any non-species dependencies in CRN.parmeter_map(rate_dependencies)
 - extended port mapping as described below
 - CRN should be immutable and additive/extenable
 - set sensible crn.params['t0'] bounds with injectors

nanosuite/jupyter.py
@@ -5,7 +5,7 @@ This module requires IPython and matplotlib
 import base64
 from copy import deepcopy
 import io
-from typing import Any, Callable
+from typing import Any, Callable, Iterable, Sequence
 from IPython.display import display, HTML  # type: ignore
 import lmfit                               # type: ignore
 from matplotlib import colormaps           # type: ignore
@@ -14,10 +14,11 @@ import xarray as xr                        # type: ignore
 from . import crn, mars
 
 
-def gradient(dataset):
+def gradient(dataset: Sequence[Any], cmap: str = 'rainbow') -> Iterable[tuple]:
+    """Color gradient for a given sequence"""
     size = len(dataset)
     for idx, _ in enumerate(dataset):
-        yield colormaps['rainbow'](idx/size)
+        yield colormaps[cmap](idx/size)
 
 
 ####################################################################################################
@@ -132,7 +133,8 @@ class Assay(mars.Assay):
         return buf.read()
 
     def _repr_html_(self, **kwargs) -> str:
-        return f'<img src="data:image/png;base64,{base64.b64encode(self._repr_png_(**kwargs)).decode()}">'
+        data = base64.b64encode(self._repr_png_(**kwargs)).decode()
+        return f'<img src="data:image/png;base64,{data}">'
 
 # monkey patches
 mars.Assay = Assay                   # type: ignore

nanosuite/mars.py
@@ -89,7 +89,7 @@ class Assay:
             if setup:
                 raise ValueError("Arguments setup_file and setup are mutually exclusive")
             if sample_map:
-                raise ValueError("Arguemnts setup_file and sample_map are mutually exclusive")
+                raise ValueError("Arguments setup_file and sample_map are mutually exclusive")
             self.setup = self.read_setup(setup_file)
         elif isinstance(setup, xr.DataArray):
             if groups:
@@ -132,9 +132,9 @@ class Assay:
         The excel file needs to contain a worksheet named
         "sample_preparations" with the following format:
 
-        Group | Sample ID | Negative  | Positive   | Buffer | Buffer % | ... | Gate  | Gate conc (nM) | ...
-        ------+-----------+-----------+------------+--------+----------+-----+-------+----------------+-----
-        ...   | Sample X1 | Sample X1 | Sample X24 |TRIS    | 100      | ... | Gate1 | 100            | ...
+        Group | Sample ID | Negative  | Positive   | ... | Gate  | Gate conc (nM) | ...
+        ------+-----------+-----------+------------+-----+-------+----------------+-----
+        ...   | Sample X1 | Sample X1 | Sample X24 | ... | Gate1 | 100            | ...
 
         There can be an arbitrary number of Buffer/Media components as well
         as chemical species. Concentrations of the latter can be provided in
@@ -152,13 +152,10 @@ class Assay:
         factors = {'mM': 1e-3, 'uM': 1e-6, 'nM': 1e-9, 'pM': 1e-12, 'fM': 1e-15, 'aM': 1e-18}
         self.sample_map = df[df.columns[-2*len(units)::2]].set_index(content)
         self.sample_map.replace([np.nan], [None], inplace=True)
-        concs = df[df.columns[1-2*len(units)::2]].set_index(content)  # FIXME: this should be cast to float
+        concs = df[df.columns[1-2*len(units)::2]].set_index(content)
         concs = concs.rename(columns=dict(zip(concs.columns, self.sample_map.columns)))
         fac = np.array([factors[u] for u in units])
         concs *= fac
-        # FIXME: decide whether this is placed well here...
-        # self.abstract_species_classes = [cls for cls in self.sample_map.columns
-        #                                  if cls not in set(self.sample_map.values.flatten())]
         for species_class in self.sample_map.columns:
             alternatives = pd.Series(self.sample_map[species_class].unique())
             for species in alternatives:
@@ -167,13 +164,13 @@ class Assay:
                 concs[species] = concs[self.sample_map[species_class]==species][species_class]
         concs.fillna(0., inplace=True)
 
+        # TODO: should controls be optional?
         return xr.DataArray(
             concs,
             {'content': content, 'species': concs.columns},
             attrs=attrs
         ).assign_coords(positive=('content', df['Positive'].values),
-                        negative=('content', df['Negative'].values))  # FIXME: should controls be optional?
-
+                        negative=('content', df['Negative'].values))
 
     def read_rfu(self, rfu_file: str,
                  groups: dict[str, list[str]|slice]) -> tuple[xr.DataArray,  # all_rfu
@@ -378,12 +375,14 @@ class Assay:
 
     @property
     def plate(self):
+        """Deprecated: use Assay.rfu instead"""
         warnings.warn("Assay.plate is deprecated. Use Assay.rfu instead",
                       DeprecationWarning, stacklevel=2)
         return self.rfu
 
     @property
     def full_plate(self):
+        """Deprecated: use Assay.all_rfu instead"""
         warnings.warn("Assay.full_plate is deprecated. Use Assay.full_rfu instead",
                       DeprecationWarning, stacklevel=2)
         return self.all_rfu
@@ -432,7 +431,7 @@ class Assay:
             array.loc[..., species] = values
         return array
 
-    def calibrate(
+    def calibrate(  # pylint: disable=too-many-arguments
         self, pos_conc: xr.DataArray, neg_conc: xr.DataArray,
         pos_rfu: xr.DataArray|None = None,
         neg_rfu: xr.DataArray|None = None, *,
@@ -476,7 +475,7 @@ class Assay:
         raise ValueError(f"""Unsupported calibration method: '{method}'
         Needs to be one of: direct [default], relaxation""")
 
-    def calibrate_relaxation(
+    def calibrate_relaxation(   # pylint: disable=too-many-arguments
         self, pos_conc: xr.DataArray, neg_conc: xr.DataArray,
         pos_rfu: xr.DataArray|None = None,
         neg_rfu: xr.DataArray|None = None, *,

nanosuite/utils.py
@@ -1,3 +1,4 @@
+"""Utilities for nanosuite"""
 from typing import Any, Callable
 from functools import wraps
 

commit a3283fdc128a74eb9dd644ec516e8a14db374717

Fix bug in crn.PartitionedCRN.state
The error was introduced in commit f704ac141f60d7ad8eebecd42c

nanosuite/crn.py
@@ -931,7 +931,7 @@ class PartitionedCRN(CRN):
         if len(state.dims) == 1:
             return xr.DataArray(state.values @ self.split_species(), state.coords)
         content_dim = next(iter(state.coords))
-        return xr.DataArray([sample.values @ self.split_species(state.coords[content_dim].data)
+        return xr.DataArray([sample.values @ self.split_species(sample.coords[content_dim].data)
                                  for sample in state],
                                 state.coords)
 