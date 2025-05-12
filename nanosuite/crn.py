harfel/nanosuite

commit 5501bdc2e2eb67e8934b5c2d171ee4dac0cc1175

Internally represent all reactions as reversible

nanosuite/crn.py
@@ -46,20 +46,20 @@ class CRN:
     species: pandas.Index
         species names in state vector
     complexes: list of (species, stoichiomentry) pairs
-    reactions: mapping of complex pairs to rate constant names
+    reactions: mapping of complex pairs to a two-tuple of rate constant names
     params: lmfit.Parameters instance of rate constants
     """
     # TODO: support open networks and buffered species
 
     species: pd.Index
     complexes: list[Reactants]
-    reactions: dict[tuple[Reactants, Reactants], str]
+    reactions: dict[tuple[Reactants, Reactants], tuple[str, str]]
     params: lmfit.Parameters
 
     def __init__(self,
                  reactions: list[tuple[Reactants, Reactants, lmfit.Parameter]]|None = None,
                  species: Iterable[str]|None = None):
-        """Create an chemical reaction network.
+        """Create a chemical reaction network.
 
         Parameters
         ----------
@@ -79,11 +79,15 @@ class CRN:
             self.add_reaction(*reaction)
 
     def __str__(self) -> str:
-        def render(complexes, name):
+        def render(complexes: tuple[Reactants, Reactants], forward: str, backward: str = '') -> str:
             educts = self._render_reactants(complexes[0])
             products = self._render_reactants(complexes[1])
-            return f"{educts} -> {products}; {name}={self.params[name].value}"
-        return '\n'.join(render(reaction, name) for reaction, name in self.reactions.items())
+            if backward:
+                return (f"{educts} <=> {products}; "
+                        + f"{forward}={self.params[forward].value}, "
+                        + f"{backward}={self.params[backward].value}")
+            return f"{educts} -> {products}; {forward}={self.params[forward].value}"
+        return '\n'.join(render(reaction, *name) for reaction, name in self.reactions.items())
 
     def _repr_html_(self) -> str:
         return (
@@ -91,22 +95,30 @@ class CRN:
             + '\n'.join(
                 f'''<tr>
                     <td style="text-align: right">{self._render_reactants(reaction[0])}</td>
+                    <td style="text-align: center">&rlhar</td>
+                    <td style="text-align: left">{self._render_reactants(reaction[1])}</td>
+                    <td style="text-align: left">{forward} = {self.params[forward].value:.2g}</td>
+                    <td style="text-align: left">{backward} = {self.params[backward].value:.2g}</td>
+                </tr>''' if backward else
+                f'''<tr>
+                    <td style="text-align: right">{self._render_reactants(reaction[0])}</td>
                     <td style="text-align: center">&LongRightArrow;</td>
                     <td style="text-align: left">{self._render_reactants(reaction[1])}</td>
-                    <td style="text-align: left">{name} = {self.params[name].value:.2g}</td>
+                    <td style="text-align: left" colspan="2">{forward} = {self.params[forward].value:.2g}</td>
                 </tr>'''
-                for reaction, name in self.reactions.items()
+                for reaction, (forward, backward) in self.reactions.items()
             )
             + '</table>'
         )
 
     @property
-    def burst_reactions(self) -> dict[tuple[Reactants, Reactants], str]:
+    def burst_reactions(self) -> dict[tuple[Reactants, Reactants], tuple[str, str]]:
         """Return subset of reactions with infinite rate constant"""
         return {
-            reaction: rate
-            for reaction, rate in self.reactions.items()
-            if self.params[rate].value == float('inf')
+            reaction: (foward_rate, backward_rate)
+            for reaction, (foward_rate, backward_rate) in self.reactions.items()
+            if self.params[foward_rate].value == float('inf')
+            or (backward_rate and self.params[backward_rate].value == float('inf'))
         }
 
     @property
@@ -140,20 +152,20 @@ class CRN:
         A 2D numpy array denoting reaction rate constants among reaction
         complexes.
         """
-        def get_rate_constant(educts, products):
-            value = (self.params[name]
-                     if (name := self.reactions.get((educts, products), ''))
-                     else 0.)
+        n = len(self.complexes)
+        rate_constants = np.zeros((n, n))
+        for (educts, products), (forward, backward) in self.reactions.items():
+            j = self.complexes.index(educts)
+            i = self.complexes.index(products)
+            kf = self.params[forward]
+            kr = self.params.get(backward, 0.)
             if burst:
-                return 1 if value == float('inf') else 0.
-            return 0 if value == float('inf') else value
-        return np.array([
-            [
-                get_rate_constant(educts, products)
-                for educts in self.complexes
-            ]
-            for products in self.complexes
-        ])
+                rate_constants[i, j] = 1 if kf.value == float('inf') else 0.
+                rate_constants[j, i] = 1 if kr and kr.value == float('inf') else 0.
+            else:
+                rate_constants[i, j] = val if (val := kf.value) != float('inf') else 0.
+                rate_constants[j, i] = val if kr and (val := kr.value) != float('inf') else 0.
+        return rate_constants
 
     def scale_concentration_unit(self, scale_factor: float):
         """Scale reaction rate constants to new concentration unit.
@@ -162,10 +174,12 @@ class CRN:
         call crn.scale_concentration_unit(1e-9) will rescale those to
         nM^-1s^-1.
         """
-        for reaction, name in self.reactions.items():
-            self.params[name].value *= scale_factor**(len(reaction[0])-1)
+        for reaction, (forward, backward) in self.reactions.items():
+            self.params[forward].value *= scale_factor**(len(reaction[0])-1)
+            self.params[backward].value *= scale_factor**(len(reaction[1])-1)
 
-    def add_reaction(self, educts: Reactants, products: Reactants, rate: lmfit.Parameter):
+    def add_reaction(self, educts: Reactants, products: Reactants,
+                     forward_rate: lmfit.Parameter, backward_rate: lmfit.Parameter|None = None):
         """Add a reaction to the network.
 
         Any novel species that occur among the reactants are automatically
@@ -175,23 +189,32 @@ class CRN:
         ----------
         educts: tuple of species name, stoichiometry tuples
         products: tuple of species name, stoichiometry tuples
-        rate: lmfit.Parameter of the rate constant
+        forward_rate: lmfit.Parameter of the forward rate constant
+        backward_rate: optional lmfit.Paramter of the backward rate constant (default: None)
         """
         # collect species and complexes
-        for reactants in [educts, products]:
-            self.species = self.species.append(pd.Index([
-                name for name, _ in reactants
-                if name not in self.species
-            ]))
+        if (products, educts) in self.reactions:
+            self.reactions[products, educts] = (self.reactions[products, educts][0],
+                                                forward_rate.name)
 
-        for compl in [educts, products]:
-            if compl not in self.complexes:
-                self.complexes.append(compl)
+        else:
+            for reactants in [educts, products]:
+                self.species = self.species.append(pd.Index([
+                    name for name, _ in reactants
+                    if name not in self.species
+                ]))
 
-        self.reactions[educts, products] = rate.name
+            for compl in [educts, products]:
+                if compl not in self.complexes:
+                    self.complexes.append(compl)
 
-        if rate.name not in self.params:
-            self.params.add(rate)
+            self.reactions[educts, products] = (forward_rate.name,
+                                                (backward_rate.name if backward_rate else None))
+
+        if forward_rate.name not in self.params:
+            self.params.add(forward_rate)
+        if backward_rate and backward_rate.name not in self.params:
+            self.params.add(backward_rate)
 
     def __getitem__(self, name: str) -> lmfit.Parameter:
         return self.params[name]
@@ -293,6 +316,18 @@ class CRN:
             return -Z @ L @ np.exp(tmp)
 
         return kinetics
+
+    def equilibrate(self, initial_condition: xr.DataArray|dict, **options) -> xr.DataArray:
+        """Equilibrium state of the reaction network
+
+        Parameters
+        ----------
+        initial_condition: xarray.DataArray
+            state vector to equilibrate
+        """
+        raise RuntimeError("FIXME: not implemented")
+        # See https://chemistry.stackexchange.com/questions/153869/numerically-solving-chemical-equilibrium-equations#answer-177284
+        # for a nice approach
 
     def integrate(self, initial_condition: xr.DataArray|dict,  # pylint: disable=invalid-name
                   t_eval: Iterable[float]|float|None = None,
@@ -651,10 +686,6 @@ def from_string(string: str, species: list[str]|None = None) -> CRN|PartitionedC
         crn = CRN(species=species)
 
     for reaction in crn_def.reactions:
-        if isinstance(reaction, crn_parser.Reaction):
-            crn.add_reaction(*reaction)
-        else:
-            crn.add_reaction(reaction.educts, reaction.products, reaction.forward)
-            crn.add_reaction(reaction.products, reaction.educts, reaction.backward)
+        crn.add_reaction(reaction.educts, reaction.products, reaction.forward, reaction.backward)
 
     return crn

nanosuite/crn_parser.py
@@ -119,7 +119,7 @@ def p_crn(p):
            | statement crn
     """
     species_defs = {} if len(p) == 2 else dict(p[2].species_defs)
-    if isinstance(p[1], (Reaction, BalancedReaction)):
+    if isinstance(p[1], Reaction):
         if len(p) == 2:
             p[0] = CrnDef([p[1]], species_defs)
         else:
@@ -152,17 +152,17 @@ def p_reaction(p):
     if len(p) == 4 and p[2] == t_RIGHT_ARROW:
         name = f'_k_{len(p.parser.context)+1}'
         p.parser.context[name] = lmfit.Parameter(name, value=1, min=0)
-        p[0] = Reaction(educts=p[1], products=p[3], rate=name)
+        p[0] = Reaction(educts=p[1], products=p[3], forward=name, backward=None)
     elif len(p) == 6:
-        p[0] = Reaction(educts=p[1], products=p[3], rate=p[5])
+        p[0] = Reaction(educts=p[1], products=p[3], forward=p[5], backward=None)
     elif len(p) == 4 and p[2] == t_DOUBLE_ARROW:
         fname = f'_k_{len(p.parser.context)+1}'
         bname = f'_k_{len(p.parser.context)+2}'
         p.parser.context[fname] = lmfit.Parameter(fname, value=1, min=0)
         p.parser.context[bname] = lmfit.Parameter(bname, value=1, min=0)
-        p[0] = BalancedReaction(educts=p[1], products=p[3], forward=fname, backward=bname)
+        p[0] = Reaction(educts=p[1], products=p[3], forward=fname, backward=bname)
     else:
-        p[0] = BalancedReaction(educts=p[1], products=p[3], forward=p[5], backward=p[7])
+        p[0] = Reaction(educts=p[1], products=p[3], forward=p[5], backward=p[7])
 
 def p_reactants(p):
     """reactants : species
@@ -281,9 +281,7 @@ def p_error(t):
 FractionDef = namedtuple('FractionDef', ['suffix', 'fraction'])
 SpeciesDef = namedtuple('SpeciesDef', ['species', 'subspecies', 'remains'])
 Species  =namedtuple('Species', ['name', 'suffix'])
-Reaction = namedtuple('Reaction', ['educts', 'products', 'rate'])
-BalancedReaction = namedtuple('BalancedReaction',
-                               ['educts', 'products', 'forward', 'backward'])
+Reaction = namedtuple('Reaction', ['educts', 'products', 'forward', 'backward'])
 CrnDef = namedtuple('CrnDef', ['reactions', 'species_defs'])
 
 
@@ -297,15 +295,12 @@ def replace_name_placeholders(crn_def, variables):
                   if (idx_str:=str(idx)) not in bound_nums]
 
     for reaction in crn_def.reactions:
-        if isinstance(reaction, Reaction):
-            if not reaction.rate.startswith('_'):
-                continue
-            variables[reaction.rate].name = f'k{free_nums.pop(0)}'
-        else:
-            if not reaction.forward.startswith('_'):
-                continue
-            num = free_nums.pop(0)
-            variables[reaction.forward].name = f'kf{num}'
+        if not any(name and name.startswith('_') for name in (reaction.forward, reaction.backward)):
+            continue
+        num = free_nums.pop(0)
+        if reaction.forward.startswith('_'):
+            variables[reaction.forward].name = f'kf{num}' if reaction.backward else f'k{num}'
+        if reaction.backward and reaction.backward.startswith('_'):
             variables[reaction.backward].name = f'kb{num}'
 
     for species_def in crn_def.species_defs.values():
@@ -346,10 +341,8 @@ def parse(string: str) -> CrnDef:
         )
 
     crn_def = CrnDef([Reaction(stratify_species(rct.educts), stratify_species(rct.products),
-                               parser.context[rct.rate])
-                      if isinstance(rct, Reaction) else
-                      BalancedReaction(stratify_species(rct.educts), stratify_species(rct.products),
-                                       parser.context[rct.forward], parser.context[rct.backward])
+                               parser.context[rct.forward],
+                               parser.context[rct.backward] if rct.backward else None)
                       for rct in crn_def.reactions],
                      {species: SpeciesDef(species,
                                           {f'{species}_{suffix}': parser.context[frac]

tests/test_crn.py
@@ -20,6 +20,17 @@ def test_params_add():
     assert 'dG' in params
     assert params['k_b'].value == params['k_f']/math.exp(-params['dG'].value)
 
+def test_reversible_encoding():
+    ode1 = crn.from_string("""
+        A <=> B; kf, kb
+    """)
+    ode2 = crn.from_string("""
+        A -> B; kf
+        B -> A; kb
+    """)
+
+    assert ode1.reactions == ode2.reactions
+
 def test_repr_html():
     """Ensure correct HTML representation"""
     test_crn = crn.from_string("""
@@ -32,18 +43,18 @@ def test_repr_html():
             <td style="text-align: right">A_pure + B</td>
             <td style="text-align: center">&LongRightArrow;</td>
             <td style="text-align: left">C</td>
-            <td style="text-align: left">k1 = 1.1</td>
+            <td style="text-align: left" colspan="2">k1 = 1.1</td>
         </tr>
         <tr>
             <td style="text-align: right">C + D</td>
             <td style="text-align: center">&LongRightArrow;</td>
             <td style="text-align: left">B + E</td>
-            <td style="text-align: left">k2 = 1.2</td>
+            <td style="text-align: left" colspan="2">k2 = 1.2</td>
         </tr><tr>
             <td style="text-align: right">A_impure + D</td>
             <td style="text-align: center">&LongRightArrow;</td>
             <td style="text-align: left">E</td>
-            <td style="text-align: left">k = inf</td>
+            <td style="text-align: left" colspan="2">k = inf</td>
         </tr></table>'''
     assert rep == ''.join(line.strip() for line in expected.split('\n'))
 
@@ -60,7 +71,7 @@ def test_from_string_reversible():
     test_crn = crn.from_string("""
         A + B <=> C
     """)
-    assert len(test_crn.reactions) == 2
+    assert len(test_crn.reactions) == 1
     assert 'kf1' in test_crn.params
     assert 'kb1' in test_crn.params
 
@@ -102,9 +113,9 @@ def test_from_string_repeated_reversible_rates():
         A + B <=> C; kf1 = 1e7, 1e3
         C + D <=> E; kf1, kb = 1e3
         """)
-    assert len(model.params) == 4
-    k1 = model.reactions[(('A', 1), ('B', 1)), (('C', 1),)]
-    k2 = model.reactions[(('C', 1), ('D', 1)), (('E', 1),)]
+    assert len(model.params) == 4  # three rate constants plus t0
+    k1 = model.reactions[(('A', 1), ('B', 1)), (('C', 1),)][0]
+    k2 = model.reactions[(('C', 1), ('D', 1)), (('E', 1),)][0]
     assert k1 == k2
 
 def test_implicit_rate_names():
@@ -237,7 +248,8 @@ def test_from_string_real_formats(value):
 
 def test_add_reaction_respects_catalysts():
     network = crn.CRN()
-    network.add_reaction(educts=(('A', 1), ('C', 1)), products=(('Z', 1), ('C', 1)), rate=lmfit.Parameter('k'))
+    network.add_reaction(educts=(('A', 1), ('C', 1)), products=(('Z', 1), ('C', 1)),
+                         forward_rate=lmfit.Parameter('k'))
     assert len(network.species) == 3
 
 def test_integrate_teval_is_optional():

commit 00a0bd64d3ab0f699b1ac4dc4b41189b758fb616

Fix reaction argument to CRN.__init__
Constructing a CRN manually (i.e. not using crn.from_string) with
initial reactions had been broken since commit
5501bdc2e2eb67e8934b5c2d171ee4dac0cc1175 (unreleased).

TODO
@@ -1,8 +1,6 @@
-- fix doctests
 - update and add nanosuite/examples
 - pass skipped tests
 = 0.3.0
-- make CRN.parametrize_for idempotent
 - permit scalar pos_rfu, neg_rfu arguments in addition to 1D/2D arrays
 - allow buffer, media, or any non-species dependencies in CRN.parmeter_map(rate_dependencies)
 - allow to turn jupyter interactive mode on or off

nanosuite/crn.py
@@ -240,10 +240,10 @@ class CRN:
     k_backward can be created with:
 
     >>> from lmfit import Parameter
-    >>> crn = CRN([
-    ...     ((("A", 1), ("B", 1)), (("C", 1),),
-    ...     Parameter('k_forward', 0.1), Parameter('k_backward', 0.1)),
-    ... ])
+    >>> crn = CRN({
+    ...     ((("A", 1), ("B", 1)), (("C", 1),)):
+    ...         (Parameter('k_forward', 0.1), Parameter('k_backward', 0.1)),
+    ... })
 
     This is equivalent to the more convenient function:
 
@@ -267,7 +267,7 @@ class CRN:
     params: ParameterMap
 
     def __init__(self,
-                 reactions: list[tuple[Reactants, Reactants, lmfit.Parameter]]|None = None,
+                 reactions: dict[tuple[Reactants, Reactants], tuple[lmfit.Parameter, lmfit.Parameter|None]]|None = None,
                  species: Iterable[str]|None = None):
         """Create a chemical reaction network.
 
@@ -286,8 +286,8 @@ class CRN:
         self.reactions = {}
         self.params = ParameterMap()
         self.params.add('t0', value=DEFAULT_INTEGRATION_START, min=DEFAULT_MIN_T0, vary=True)
-        for reaction in reactions or []:
-            self.add_reaction(*reaction)
+        for reaction, rates in (reactions or {}).items():
+            self.add_reaction(*reaction, *rates)
 
     def __str__(self) -> str:
         def render(complexes: tuple[Reactants, Reactants], forward: str, backward: str = '') -> str:
@@ -889,17 +889,20 @@ class PartitionedCRN(CRN):
     To declare subspecies compositions, PartitionedCRN offers the method
     define_subspecies:
 
-    >>> reactions = from_string(
-    ...     "biomarker_mutant + probe -> biomarker_mutant + signal").reactions
-    >>> crn = PartitionedCRN(reactions)
+    >>> from lmfit import Parameter
+    >>> crn = PartitionedCRN({
+    ...     ((("biomarker_mutant", 1), ("probe", 1)), (("biomarker_mutant", 1), ('signal', 1))):
+    ...         (Parameter('k', 0.1), None),
+    ... })
+
     >>> crn.define_subspecies("biomarker",
-    ...     subspecies={"mutant": 0.01}, rest="wildtype")
+    ...     subspecies={"mutant": Parameter('k_mutant', 0.01)}, rest="biomarker_wildtype")
 
     >>> initial = crn.state(biomarker=100)
     >>> trajectory = crn.integrate(initial)
-    >>> total = trajecory.sel(species="biomarker")
-    >>> mutant = trajecory.sel(species="biomarker_mutant")
-    >>> wildtype = trajecory.sel(species="biomarker_wildtype")
+    >>> total = trajectory.sel(species="biomarker")
+    >>> mutant = trajectory.sel(species="biomarker_mutant")
+    >>> wildtype = trajectory.sel(species="biomarker_wildtype")
 
 
     Internally, the mapping from species space to subspecies space is
@@ -924,7 +927,7 @@ class PartitionedCRN(CRN):
 
 
     def __init__(self,
-                 reactions: list[tuple[Reactants, Reactants, lmfit.Parameter]]|None = None,
+                 reactions: dict[tuple[Reactants, Reactants], tuple[lmfit.Parameter, lmfit.Parameter|None]]|None = None,
                  species_defs : list[tuple[str, dict[str, lmfit.Parameter], str]]|None = None,
                  species: Iterable[str]|None = None):
         super().__init__(reactions, species)