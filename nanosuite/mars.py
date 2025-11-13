162309de6ac788029bcae5bf7d8dd12d90ee673d
harold <dadapapa@gmail.com>
2025-11-13 13:37:05 +0000

    Convert species in setup_file into valid names
    Subspecies names in the setup_file are now normalized before they
    are interpreted as parameters. For example, the name "A-1" could
    lead to problems when used to specialize a model parameter (as
    lmfit.Parameter names are not allowed to contain '-'). Now, any
    character that does not match a-zA-Zα-ωΑ-Ω0-9_ is converted into '_'.

--- nanosuite/mars.py
+++ nanosuite/mars.py
@@ -154,8 +154,10 @@ class Assay:
         df = df[df.columns[2:]]
         units = [match[1] for s in df.columns[1::2] if (match:=re.match(r'.*\(([munpfa]M)\)', s))]
         factors = {'mM': 1e-3, 'uM': 1e-6, 'nM': 1e-9, 'pM': 1e-12, 'fM': 1e-15, 'aM': 1e-18}
-        self.sample_map = df[df.columns[-2*len(units)::2]].set_index(content)
-        self.sample_map.replace([np.nan], [None], inplace=True)
+        sample_map = df[df.columns[-2*len(units)::2]].set_index(content)
+        sample_map.replace([np.nan], [None], inplace=True)
+        sample_map.replace('[^a-zA-Zα-ωΑ-Ω0-9_]', '_', regex=True, inplace=True)
+        self.sample_map = sample_map
         concs = df[df.columns[1-2*len(units)::2]].set_index(content)
         concs = concs.rename(columns=dict(zip(concs.columns, self.sample_map.columns)))
         fac = np.array([factors[u] for u in units])
